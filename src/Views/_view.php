cb994691ab166336b83c261e114d467e2c05375f
Guilherme Ortiz Santos <82048315+GuilhermeOrtizAluno@users.noreply.github.com>
2021-12-10 16:22:12 -0300

    Delete src/Views directory

--- src/Views/_view.php
+++
@@ -1,7 +0,0 @@
-<?php include('layout/header.php'); ?>
-
-<main>
-    <?= $v->section("content") ?>
-</main>
-
-<?php include("layout/footer.php"); ?>
--- src/Views/home.php
+++
@@ -1,3 +0,0 @@
-<?php $v->layout("_view"); ?>
-
-<h1>Seja Bem-Vindo <?= $_SESSION["name"] . " ". $_SESSION["last"] ?></h1>
